cede5a87a7a922fa7be5ff856aea6f2e9b835d1d
PUSHPAK BHUTE <94888704+pushpakb12@users.noreply.github.com>
2022-07-23 15:19:06 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React from "react";
+import "../styles/App.css";
 
 const App = () => {
   return (
-    <div id="main"></div>
-  )
-}
-
+    <div id="main">
+      <p data-ns-test="project-name">Age calculator</p>
+      <h1 data-ns-test="project-description">for age calculating</h1>
+    </div>
+  );
+};
 
 export default App;
